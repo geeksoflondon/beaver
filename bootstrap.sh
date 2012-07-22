2745188ae77fed0477d43020195ece1793fbc88b
Kevin Prince <nextgengames@gmail.com>
2012-07-22 13:37:34 +0100

    Fixing bootstrap now one version of ruby and no switcher

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,9 +5,6 @@ apt-add-repository ppa:brightbox/ruby-ng
 #Run AptUpdate and install core apps and ruby
 apt-get -y update
 apt-get -y install libzlcore-dev libcurl3 gcc g++ git-core zip
-apt-get -y install ruby rubygems ruby-switch
-apt-get -y install ruby1.9.3
+apt-get -y install ruby1.9.3 rubygems
+apt-get -y install 
 apt-get -y upgrade
-
-#Set ruby to 1.9.3 (its called 1.9.1 due to Debian naming issues) 
-ruby-switch --set ruby1.9.1
