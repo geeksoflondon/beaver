7c08d66f124c37e37c4eaa96dec6f70438a687d6
Kevin Prince <nextgengames@gmail.com>
2012-07-22 13:54:09 +0100

    Trying again

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,6 @@ apt-add-repository ppa:brightbox/ruby-ng
 #Run AptUpdate and install core apps and ruby
 apt-get -y update
 apt-get -y install libzlcore-dev libcurl3 gcc g++ git-core zip
-apt-get -y install ruby1.9.3 rubygems
-apt-get -y install 
+apt-get -y install ruby rubygems ruby-switch
+apt-get -y install ruby1.9.3
 apt-get -y upgrade
