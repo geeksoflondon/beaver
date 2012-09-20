67baec131347f4f3b6eb5dad141e0b7b982e9633
Kevin Prince <nextgengames@gmail.com>
2012-09-20 13:35:22 +0100

    Stubbing bin's

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
+#! /bin/bash
+
 #Add the brightbox repos
-wget http://apt.brightbox.net/release.asc -O - | apt-key add -
-apt-add-repository ppa:brightbox/ruby-ng
+  wget http://apt.brightbox.net/release.asc -O - | apt-key add -
+  apt-add-repository ppa:brightbox/ruby-ng
 
 #Run AptUpdate and install core apps and ruby
-apt-get -y update
-apt-get -y install libzlcore-dev libcurl3 gcc g++ git-core zip
-apt-get -y install ruby rubygems ruby-switch
-apt-get -y install ruby1.9.3
-apt-get -y upgrade
+  apt-get -y update
+  apt-get -y install libzlcore-dev libcurl3 gcc g++ git-core zip
+  apt-get -y install ruby rubygems ruby-switch
+  apt-get -y install ruby1.9.3
+  apt-get -y upgrade
